23cff5309a274adb894a1d88efe05eba0126622b
jin4363 <zxcwlsgh12@naver.com>
2021-06-06 21:47:59 +0900

    api complete ok

--- android/Preoder/app/src/main/java/com/sample/kakao/Nmaps_test.java
+++ android/Preoder/app/src/main/java/com/sample/kakao/Nmaps_test.java
@@ -7,14 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.InfoWindow;
+import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
 
 import org.jetbrains.annotations.NotNull;
@@ -29,78 +33,103 @@ public class Nmaps_test extends AppCompatActivity {
     private MapView mapView;
     private static final int LOCATION_PERMISSION_REQUEST_CODE =1000;
     private FusedLocationSource locationSource;
-    private NaverMap naverMap;
+    private static NaverMap naverMap;
     private Geocoder geocoder ;
-    private String json_str;
+    private static String json_str;
     private MyAsyncTask MA = new MyAsyncTask();
     private MyAsyncTask2 MA2 = new MyAsyncTask2();
-    private ArrayList<String>R_nameList;
-    private ArrayList<String>R_locationList;
-    private ArrayList<Double>R_latitudeList;
-    private ArrayList<Double>R_longitudeList;
-
+    private static ArrayList<String>R_nameList;
+    private static ArrayList<String>R_locationList;
+    private static ArrayList<Double>R_latitudeList;
+    private static ArrayList<Double>R_longitudeList;
+    Intent intent;
     String my_location_str;
-    Button near_list_btn,get_json_btn,parse_btn;
+    Button search_btn;
     TextView tv,tv2,tv3;
     EditText editText;
     Double my_latitude,my_longitude;
+    private static double[] lati_arr,longi_arr;
+    private static String[] name,addr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nmaps_test);
-        get_json_btn = (Button)findViewById(R.id.get_json_btn);
-        near_list_btn = findViewById(R.id.near_list_btn);
+
+        search_btn = findViewById(R.id.search_btn);
         tv = findViewById(R.id.tv);
         tv2 = findViewById(R.id.tv2);
         tv3= findViewById(R.id.tv3);
         editText = findViewById(R.id.editText);
-        parse_btn = findViewById(R.id.parse_btn);
+
+        //지도 사용권한 받기
+        locationSource = new FusedLocationSource(this,LOCATION_PERMISSION_REQUEST_CODE);
         mapView = (MapView)findViewById(R.id.map_view);
+//        Marker test_mk = new Marker();
+//        test_mk.setPosition(new LatLng(35.107,128.967));
+//        test_mk.setMap(naverMap);
+
+
+        intent = getIntent();
+        R_nameList = (ArrayList<String>)intent.getSerializableExtra("name_list");
+        if(R_nameList !=null && getIntent() !=null)
+        {
+            //tv.setText(name[0]);
+
+            R_locationList = (ArrayList<String>)intent.getSerializableExtra("addr_list");
+            R_latitudeList = (ArrayList<Double>)intent.getSerializableExtra("lati_list");
+            R_longitudeList = (ArrayList<Double>)intent.getSerializableExtra("longi_list");
+
+
+
+
+        }else{
+            R_nameList = new ArrayList<>();
+            R_locationList = new ArrayList<>();
+            R_latitudeList = new ArrayList<>();
+            R_longitudeList = new ArrayList<>();
+
+        }
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this::OnMapReady);
 
-        R_nameList = new ArrayList<>();
-        R_locationList = new ArrayList<>();
-        R_latitudeList = new ArrayList<>();
-        R_longitudeList = new ArrayList<>();
-        //지도 사용권한 받기
-        locationSource = new FusedLocationSource(this,LOCATION_PERMISSION_REQUEST_CODE);
-        geocoder = new Geocoder(mapView.getContext());
-        get_json_btn.setOnClickListener(new View.OnClickListener() {
+        search_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                my_latitude = locationSource.getLastLocation().getLatitude();
-                my_longitude = locationSource.getLastLocation().getLongitude();
-                //tv2.setText(my_latitude.toString()+"\n"+my_longitude.toString());
-                //자신의 위치 저장
-                try {
-                    my_location_str = geocoder.getFromLocation(my_latitude,my_longitude,1).toString();
-                    //tv.setText(my_location_str);
-                    String[] str_arr = my_location_str.split("=");
-                    String[] str_arr_ = str_arr[6].split(",");
-                    my_location_str = str_arr_[0];  //xx동
-                    //tv2.setText(my_location_str);
-                }catch(Exception e)
-                {
-                    e.printStackTrace();
-                }
-
                 Thread thread = new Thread(){
                     public void run(){
-                        if(editText.getText().toString().equals("맥도날드") || editText.getText().toString().equals("롯데리아"))
+                        geocoder = new Geocoder(mapView.getContext());
+                        my_latitude = locationSource.getLastLocation().getLatitude();
+                        my_longitude = locationSource.getLastLocation().getLongitude();
+                        //tv2.setText(my_latitude.toString()+"\n"+my_longitude.toString());
+                        //자신의 위치 저장
+                        try {
+                            my_location_str = geocoder.getFromLocation(my_latitude,my_longitude,1).toString();
+                            //tv.setText(my_location_str);
+                            String[] str_arr = my_location_str.split("=");
+                            String[] str_arr_ = str_arr[6].split(",");
+                            my_location_str = str_arr_[0];  //xx동
+                            //tv2.setText(my_location_str);
+                        }catch(Exception e)
                         {
-                            MA.set_input(my_location_str+editText.getText().toString());
+                            e.printStackTrace();
+                        }
+                        if(editText.getText().toString().equals("맥도날드") || editText.getText().toString().equals("롯데리아")||editText.getText().toString().equals("맘스터치"))
+                        {
+                            MA.set_input(my_location_str+" "+editText.getText().toString());
                         }else
                         {
                             MA.set_input(editText.getText().toString());
                         }
-                        MA.set_input(editText.getText().toString());
+                        //MA.set_input(editText.getText().toString());
                         MA.doInBackground();
+
                         json_str = MA.get_json();
                         //tv.setText(json_str);
                         JSONParse(json_str);    //String list 배열에 저장
+
+
                         //tv.setText(R_nameList.toString() + "\n" + R_locationList.toString());
                         //R_namList, R_locationList 만들어짐
                         if(!R_locationList.isEmpty())
@@ -111,8 +140,9 @@ public class Nmaps_test extends AppCompatActivity {
                                 //MA2.set_input(R_locationList.get(i));
                                 MA2.doInBackground();
                                 json_str = MA2.get_json();
-                                JSONParse2(json_str);
+                                JSONParse2(json_str);    //String list 배열에 저장
                             }
+
                             //tv2.setText(R_latitudeList.toString() + "\n" + R_longitudeList.toString());
                         }else
                         {
@@ -124,24 +154,22 @@ public class Nmaps_test extends AppCompatActivity {
                         intent.putExtra("addr_list",R_locationList);
                         intent.putExtra("lati_list",R_latitudeList);
                         intent.putExtra("longi_list",R_longitudeList);
-                        startActivity(intent);
-//                        R_nameList.clear();
-//                        R_locationList.clear();
-//                        R_latitudeList.clear();
-//                        R_longitudeList.clear();
-                        finish();
 
-                    }
+                        startActivity(intent);
+                        R_latitudeList.clear();
+                        R_longitudeList.clear();
+                        finish();
+                    }//run()
                 };
                 thread.start();
-
             }//on click
-
         });
+
     }
 
-    public void JSONParse2(String json_str_)    //주소 결과
+    public static void JSONParse2(String json_str_)    //주소 결과
     {
+
         try{
             JSONObject jsonObject = new JSONObject(json_str_);
             JSONArray jsonArray = jsonObject.getJSONArray("addresses");
@@ -152,8 +180,8 @@ public class Nmaps_test extends AppCompatActivity {
                 if(jsonObject1.get("x") != null && jsonObject1.get("y") !=null)
                 {
 
-                    R_latitudeList.add(Double.parseDouble(jsonObject1.get("x").toString()));
-                    R_longitudeList.add(Double.parseDouble(jsonObject1.get("y").toString()));
+                    R_latitudeList.add(Double.parseDouble(jsonObject1.get("y").toString()));
+                    R_longitudeList.add(Double.parseDouble(jsonObject1.get("x").toString()));
                 }
             }
             //tv2.setText(R_latitudeList.toString() + "\n" + R_longitudeList.toString());
@@ -163,9 +191,10 @@ public class Nmaps_test extends AppCompatActivity {
         }
     }
 
-    public void JSONParse(String json_str_) //식당이름 결과
+    public static void JSONParse(String json_str_) //식당이름 결과
     {
-
+        R_nameList.clear();
+        R_locationList.clear();
         try{
             JSONObject jsonObject = new JSONObject(json_str_);
             JSONArray jsonArray = jsonObject.getJSONArray("items");
@@ -174,7 +203,7 @@ public class Nmaps_test extends AppCompatActivity {
             {
                 JSONObject jsonObject1 = jsonArray.getJSONObject(i);
                 if(jsonObject1.getString("title") != null &&
-                        jsonObject1.getString("category").equals("양식>햄버거") &&
+                        (jsonObject1.getString("category").equals("양식>햄버거") || jsonObject1.getString("category").equals("음식점>치킨,닭강정") )&&
                         jsonObject1.getString("address") != null)
                 {
                     String getTitle =  jsonObject1.getString("title");
@@ -205,6 +234,13 @@ public class Nmaps_test extends AppCompatActivity {
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
     public void OnMapReady(@NonNull NaverMap naverMap) {
         this.naverMap = naverMap;
 
@@ -215,6 +251,7 @@ public class Nmaps_test extends AppCompatActivity {
 
 
         //LatLng init_Position = new LatLng(35.106281,128.9661773);//하단역 2번출구
+
         //자신 위치로 카메라 이동
         //CameraUpdate cu = CameraUpdate.scrollTo(init_Position);
         //naverMap.moveCamera(cu);    //초기위치 하단역
@@ -226,10 +263,62 @@ public class Nmaps_test extends AppCompatActivity {
         uiSettings.setZoomControlEnabled(true); //zoom
         uiSettings.setLocationButtonEnabled(true);//현재위치 버튼
 
+        if(R_latitudeList.size()>0)
+        {
+            ArrayList<Marker> mk_arr = new ArrayList<>();
+
+
+
+            for(int i=0;i <R_latitudeList.size();i++)
+            {
+                Marker mk = new Marker();
+                if(R_latitudeList.get(i)!=null && R_longitudeList.get(i)!=null)
+                {
+                    //Toast.makeText(Nmaps_test.this,i + " on map불림",Toast.LENGTH_LONG).show();
+                    mk.setPosition(new LatLng( R_latitudeList.get(i),R_longitudeList.get(i) ));
+                    mk.setTag(R_nameList.get(i));
+                    //mk.setTag("맥도날드");
+                    //mk.setCaptionText(R_nameList.get(i));
+                    mk_arr.add(mk);
+                    //tv.setText(R_nameList.toString());
+                    //tv2.setText(R_latitudeList.toString() + "\n" + R_longitudeList.toString());
+                    Toast.makeText(Nmaps_test.this,R_latitudeList.size()+"개 표시", Toast.LENGTH_LONG).show();
+
+                }
+            }
+            InfoWindow infoWindow;
+            for(int i=0;i<mk_arr.size();i++)
+            {
+                infoWindow = new InfoWindow();
+                infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+                    @NonNull
+                    @NotNull
+                    @Override
+                    public CharSequence getText(@NonNull @NotNull InfoWindow infoWindow) {
+                        return (CharSequence)infoWindow.getMarker().getTag();
+                    }
+                });
+                mk_arr.get(i).setMap(naverMap);
+                infoWindow.open(mk_arr.get(i));
+            }
+//            for (Marker marker : mk_arr) {
+//                marker.setMap(naverMap);
+//                infoWindow.open(marker);
+//
+//            }
+
+        }
+
         //지도상에 마커찍기
-        //Marker maker = new Marker();
-        //maker.setPosition(init_Position);
-        //maker.setMap(naverMap);
+//        Marker maker = new Marker();
+//        maker.setPosition(init_Position);
+//        maker.setMap(naverMap);
+//
+//        Marker test_mk = new Marker();
+//        test_mk.setPosition(new LatLng(35.107,128.967));
+//        test_mk.setMap(naverMap);
+
+
     }
 
 
--- android/Preoder/app/src/main/java/com/sample/kakao/restaurant_list.java
+++ android/Preoder/app/src/main/java/com/sample/kakao/restaurant_list.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -20,37 +21,70 @@ import java.util.ArrayList;
 public class restaurant_list extends AppCompatActivity {
     public static int mcdo = 0;
     public static int lotteria = 1;
+    public static int momst = 2;
     String[] name;
     String[] addr;
     Double[] latitude;
     Double[] longitude;
     String[] brand ={"맥도날드","맥도날드","롯데리아","롯데리아"};
-    Integer[] pic = {R.drawable.mcdo, R.drawable.mcdo, R.drawable.lotteria, R.drawable.lotteria};
+    Integer[] pic = {R.drawable.mcdo, R.drawable.lotteria,R.drawable.momstouch};
     Intent intent;
+    ImageButton back_btn,map_view_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_list);
+        back_btn = (ImageButton)findViewById(R.id.back_btn);
+        map_view_btn=(ImageButton)findViewById(R.id.map_view_btn);
+
+
 
         intent = getIntent();
 
         ArrayList<String> name_list = (ArrayList<String>)intent.getSerializableExtra("name_list");
-        int size = name_list.size();
-        name = new String[size];
+        name = new String[name_list.size()];
         name_list.toArray(name);
 
+
         ArrayList<String> addr_list = (ArrayList<String>)intent.getSerializableExtra("addr_list");
-        addr = new String[size];
+        addr = new String[addr_list.size()];
         addr_list.toArray(addr);
 
+        //ArrayList<Double> temp_lati_list = (ArrayList<Double>)intent.getSerializableExtra("lati_list");
         ArrayList<Double> lati_list = (ArrayList<Double>)intent.getSerializableExtra("lati_list");
-        latitude = new Double[size];
+        latitude = new Double[lati_list.size()];
         lati_list.toArray(latitude);
 
+        //ArrayList<Double> temp_longi_list = (ArrayList<Double>)intent.getSerializableExtra("longi_list");
         ArrayList<Double> longi_list = (ArrayList<Double>)intent.getSerializableExtra("longi_list");
-        longitude = new Double[size];
+        longitude = new Double[longi_list.size()];
         longi_list.toArray(longitude);
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(), Nmaps_test.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        map_view_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(), Nmaps_test.class);
+
+                intent.putExtra("name_list",name_list);
+                intent.putExtra("addr_list",addr_list);
+
+                intent.putExtra("lati_list",lati_list);
+                intent.putExtra("longi_list",longi_list);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         CustomList adapt = new CustomList(restaurant_list.this);
         ListView list = (ListView) findViewById(R.id.restaurant_listview);
@@ -83,12 +117,16 @@ public class restaurant_list extends AppCompatActivity {
             location_name.setText(name[position]);
 
             String[ ]temp = name[position].split(" ");
-            if(temp[0].equals("맥도날드"))
+
+            if(name[position].charAt(0) == '맥')
             {
                 restaurant_img.setImageResource(pic[mcdo]);
-            }else if(temp[0].equals("롯데리아"))
+            }else if(name[position].charAt(0) == '롯')
             {
                 restaurant_img.setImageResource(pic[lotteria]);
+            }else if(name[position].charAt(0) == '맘')
+            {
+                restaurant_img.setImageResource(pic[momst]);
             }
 
             sell_menu.setText("햄버거");
